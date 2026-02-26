fcd82d25525420a42eba0e7a9857b36e73a3289a
NazTM <103762441+NazTM@users.noreply.github.com>
2026-02-26 21:23:43 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+#langchain core for predictable outputs
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+  topic: str
+  summary: str
+  sources: list[str]
+  tools_used: list[str]
+
+#use gemini tbh, should be free
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm2 = ChatAnthropic(model="Claude-3-5-sonnet-20241022") #change to latest claude haiku
+
+parser= PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+  llm = llm,
+  prompt = prompt,
+  tools = tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+query = input("What can I help you with?")
+raw_response = agent_executor.invoke({"query":query})
+# print(raw_response)
+
+try:
+  structured_response = parser.parse(raw_response.get("output")[0]["text"])
+  print(structured_response)
+except Exception as e:
+  print("Error parsing response", e, "Raw Response -", raw_response)
+
